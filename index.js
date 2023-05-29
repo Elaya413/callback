3f1e5fc3cb206df93ebb45aec035121cbad595f7
Elaya413 <elayavathiselvam@gmail.com>
2023-05-29 20:07:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+//function add(a,b){
+//  return a+b
+//}
+//
+//function display(a,b,callback){
+//console.log(`The sum of ${a}+${b}=`,callback(a,b))         
+//}
+//display(5,10,add)
+//
+//
+//function removenegativenumber(x){
+//if(x<0)
+//return true
+//else
+//return flase
+//}
+//
+//function positivenumber
+
+
+
+//let h1=document.createElement('h1')
+//document.body.appendChild(h1)
+//
+//setInterval(()=>{
+//h1.innerHTML="I am Elayavthi"
+//setInterval(()=>{
+//h1.innerHTML="I am learning callback hell"
+//setInterval(()=>{
+//  h1.innerHTML="It is difficult to learning"
+//  setInterval(()=>{
+//    h1.innerHTML="Find an alternate for callback hell"
+//    setInterval(()=>{
+//      h1.innerHTML="which is promise"
+//      setInterval(()=>{
+//        h1.innerHTML="we can see promise for tomorrow"
+//      },2000)
+//},2000)
+//
+//},2000)
+//},2000)
+//},2000)
+//},2000)
+//
+
+
+
+let h1=document.createElement('h1')
+document.body.appendChild(h1)
+
+setTimeout(()=>{
+h1.innerHTML="9"
+setTimeout(()=>{
+h1.innerHTML="8"
+setTimeout(()=>{
+  h1.innerHTML="7"
+setTimeout(()=>{
+    h1.innerHTML="6"
+    setTimeout(()=>{
+      h1.innerHTML="5"
+      setTimeout(()=>{
+        h1.innerHTML="4"
+        setTimeout(()=>{
+          h1.innerHTML="3"
+          setTimeout(()=>{
+            h1.innerHTML="2"
+            setTimeout(()=>{
+              h1.innerHTML="1"
+              setTimeout(()=>{
+                h1.innerHTML="HAPPY INDEPENDENCE DAY"
+        },2000)
+      },2000)
+},2000)
+
+},2000)
+},2000)
+},2000)
+
+},2000)
+},2000)
+},2000)
+},2000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
